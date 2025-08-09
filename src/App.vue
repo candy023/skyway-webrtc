e48cba73109675ff4d2be1928b4b3ec1854bde39
candy023 <chitose0720@icloud.com>
2025-08-10 02:30:05 +0900

    Initial commit: Vue + SkyWay + Tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="p-6 bg-gray-100 text-center">
+    <h1 class="text-3xl font-bold text-indigo-600">Hello Tailwind!</h1>
+    <p class="mt-2 text-gray-700">SkyWay + Vue + Tailwind の世界へようこそ</p>
+  </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// src/main.js
+import './assets/main.css'
+
+createApp(App).mount('#app')
